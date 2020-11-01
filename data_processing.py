a10086ac408ec62e9e3ac5aca7b1669dee812000
Andrew Xi <andrew.xi@asu.edu>
2020-10-31 18:32:27 -0700

    Update CSV commands so that row and column numbers are not preserved in final x_raw and y_raw CSVs

--- data_processing.py
+++ data_processing.py
@@ -9,7 +9,7 @@ d_path = 'cumulative_2020.09.04_15.26.41.csv'
 df = pd.read_csv(d_path, header=0)
 print(df.columns.tolist())
 # removed kepler_name and koi_comment (unnecessary features) features with all nans (koi_longp, koi_ingress, koi_sage, koi_model_dof, koi_model_chisq)
-df = df.drop(['kepler_name', 'koi_comment', 'koi_longp', 'koi_ingress', 'koi_sage', 'koi_model_dof', 'koi_model_chisq',
+df = df.drop(['rowid', 'kepler_name', 'koi_comment', 'koi_longp', 'koi_ingress', 'koi_sage', 'koi_model_dof', 'koi_model_chisq',
               'koi_pdisposition', 'koi_score', 'koi_fpflag_nt', 'koi_fpflag_ss', 'koi_fpflag_co', 'koi_fpflag_ec',
               'koi_disp_prov'], axis=1)
 df = df.dropna()
@@ -17,9 +17,9 @@ df = df[df['koi_disposition'] != 'CANDIDATE']
 y = df['koi_disposition']
 codes, values = pd.factorize(y)
 codes_df = pd.DataFrame(codes)
-codes_df.to_csv('y_raw.csv')
+codes_df.to_csv('y_raw.csv', index=False, header=False)
 
-print(f'{codes}, {values}')
+# print(f'{codes}, {values}')
 
 col_with_str = df.select_dtypes(include='object').columns.tolist()
 df_nums = df
@@ -28,4 +28,4 @@ col_with_num = df_nums.columns.tolist()
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(df_nums)
 df_nums = pd.DataFrame(x_scaled)
-df_nums.to_csv('x_raw.csv')
+df_nums.to_csv('x_raw.csv', index=False, header=False)
